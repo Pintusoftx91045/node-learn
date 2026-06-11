094c6a9fbe6afce7dd2a2009e9f5fc964d096365
Pintusoftx91045 <pintusoftx@gmail.com>
2026-06-11 12:13:54 +0530

    fix(cors): update allowed origin to live frontend domain

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(
     origin: [
       "http://localhost:5173", // Vite local
       "http://localhost:3000", // Next.js local
-      "https://your-frontend-domain.vercel.app", // Live frontend URL
+      "https://booking-management-gamma.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
